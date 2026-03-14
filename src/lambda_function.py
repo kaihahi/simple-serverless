973d7707e12312263b67a3b9a3ae4c3699a8c10f
kaisei <kaisei.10.14.11@gmail.com>
2026-03-14 13:40:06 +0900

    lambda_commit

---
+++ src/lambda_function.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+import uuid
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('sample-table')
+
+def lambda_handler(event, context):
+    body = json.loads(event['body'])
+    memo = body['memo']
+
+    item = {
+        "id": str(uuid.uuid4()),
+        "memo": memo
+    }
+
+    table.put_item(Item=item)
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "saved"})
+    }
